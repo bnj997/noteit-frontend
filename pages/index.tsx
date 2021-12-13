a1db49459c13ea9de650e4a60f40757acafefe09
bnj997 <bnj997@gmail.com>
2021-12-13 11:43:39 +1100

    added logic for login,logout and register with cache updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
+import NavBar from "../components/NavBar";
+import { useMeQuery } from "../generated/graphql";
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>;
+  const { data, loading } = useMeQuery();
+  return (
+    <>
+      <NavBar />
+      <div>Hello {data?.me?.email}</div>
+    </>
+  );
 };
 
 export default Home;
