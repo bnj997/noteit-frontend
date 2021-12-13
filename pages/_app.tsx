43ccad964fa9af6d9016b45f40cf2814dad6b28d
bnj997 <bnj997@gmail.com>
2021-12-13 13:26:20 +1100

    used next apollo library for ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider, LightMode } from "@chakra-ui/react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  credentials: "include",
-  cache: new InMemoryCache(),
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider>
-        <LightMode>
-          <Component {...pageProps} />
-        </LightMode>
-      </ChakraProvider>
-    </ApolloProvider>
+    <ChakraProvider>
+      <LightMode>
+        <Component {...pageProps} />
+      </LightMode>
+    </ChakraProvider>
   );
 }
 
